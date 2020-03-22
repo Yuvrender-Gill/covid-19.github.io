a9c7a93e8d6cb44358e5635b9b36d8d2c16c5090
yuvrende <yuvrender.gill@mail.utoronto.ca>
2020-03-22 17:41:47 -0400

    Made the code more modular

---
+++ js/mapping/map.js
@@ -0,0 +1,18 @@
+/*
+* Initiate the map object with mapbox.gl
+*/
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2lsbHl1djIiLCJhIjoiY2s1OGIwbjFsMDk1YzNmcG5scnRib2o1MCJ9.gvSDdGfitit_En6w3JN-xw';
+
+
+
+
+
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10',
+    center: [105.13734351262877, 33.137451890638886],
+    zoom: 4,
+    pitch: 0
+});
+
--- js/mapping/mapFormat.js
+++ js/mapping/mapFormat.js
@@ -1,17 +1,6 @@
-// Author: Yuvrender Gill
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZ2lsbHl1djIiLCJhIjoiY2s1OGIwbjFsMDk1YzNmcG5scnRib2o1MCJ9.gvSDdGfitit_En6w3JN-xw';
-
-/*
-* Initiate the map object with mapbox.gl
-*/
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10',
-    center: [105.13734351262877, 33.137451890638886],
-    zoom: 4,
-    pitch: 0
-});
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
+
+
   

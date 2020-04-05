6a56228828d84622947aab50a60e1d83b8cd7b42
yuvrende <yuvrender.gill@mail.utoronto.ca>
2020-04-05 15:03:56 -0400

    Added the documentation for the creating of countryGeoJSON object

--- js/mapping/countryGeoJSON.js
+++ js/mapping/countryGeoJSON.js
@@ -1,8 +1,13 @@
+// Get the live data from John Hopkins University's Github account on COVID-19 cases
+
 Url_confirmed = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 Url_deaths = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
 Url_recovered = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
 
-
+/* The function converts a given CSV file to json format.
+ * @csv : A string in csv format.about-author
+ * @result: A json object created from the csv file. 
+ */
 function csvJSON(csv){
 
     var lines=csv.split("\n");
@@ -21,11 +26,18 @@ function csvJSON(csv){
     return result
 };
 
-function fetchData(theUrl)
+/* Fetches the data from a given url. Converts the fetched csv
+ * data to a json object and then pushes each object within the 
+ * JSON object to an array and returns the array.
+ * 
+ * @Url: String url of destination to get data from
+ * @arr: Array of objects converted to JSON from CSV
+*/
+function fetchData(Url)
 { 
     var arr = []
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.open( "GET", Url, false ); // false for synchronous request
     xmlHttp.send( null );
     for (var item in csvJSON(xmlHttp.responseText)){
         arr.push(csvJSON(xmlHttp.responseText)[item]);
@@ -33,20 +45,24 @@ function fetchData(theUrl)
     return arr;
 }
 
+/* Create three time series arrays for number of confirmed cases,
+ * number of deaths and number of recovered cases.
+ */
 var confirmedTseries = fetchData(Url_confirmed);
 var deathTseries = fetchData(Url_deaths);
 var recoveredTseries = fetchData(Url_recovered);
-console.log(confirmedTseries);
+
+// GeoJSON object for representation of all the time series data 
+// in geoJSON format.
 countryGeoJSON = {
 	"type" : "FeatureCollection",
 	"name" : "JSONFeature",
     "features" : []
 }
 
+// Represents the data in the geoJSON format.
 for (var entry in confirmedTseries){
    
-    
-    
     for (var entryItem in confirmedTseries[entry]){
         feature = {
             "type": "Feature",
@@ -76,58 +92,9 @@ for (var entry in confirmedTseries){
         countryGeoJSON["features"].push(feature);
     };
     
-}
+};
 
-// for (var rawEntry in deathTseries){
-  
-//     for (var jsonEntry in countryGeoJSON["features"]){
-       
-//         if ((deathTseries[rawEntry]["Province/State"] === countryGeoJSON["features"][jsonEntry]["properties"]["province"])
-//         && (deathTseries[rawEntry]["Country/Region"] === countryGeoJSON["features"][jsonEntry]["properties"]["country"])) {
-            
-//             for (var jsonDate in countryGeoJSON["features"][jsonEntry]["properties"]){
-                
-//                 for (var rawDate in deathTseries[rawEntry]){
-                    
-//                     if (jsonDate == rawDate){
-                      
-//                         countryGeoJSON["features"][jsonEntry]["properties"][jsonDate].push(parseFloat(deathTseries[rawEntry][rawDate]));
-//                     };
-//                 };
-//             };
-//         };
-//     }
-// }
-
-// for (var rawEntry in recoveredTseries){
-  
-//     for (var jsonEntry in countryGeoJSON["features"]){
-       
-//         if ((recoveredTseries[rawEntry]["Province/State"] === countryGeoJSON["features"][jsonEntry]["properties"]["province"])
-//         && (recoveredTseries[rawEntry]["Country/Region"] === countryGeoJSON["features"][jsonEntry]["properties"]["country"])) {
-            
-//             for (var jsonDate in countryGeoJSON["features"][jsonEntry]["properties"]){
-                
-//                 for (var rawDate in recoveredTseries[rawEntry]){
-                    
-//                     if (jsonDate == rawDate){
-                      
-//                         countryGeoJSON["features"][jsonEntry]["properties"][jsonDate].push(parseFloat(recoveredTseries[rawEntry][rawDate]));
-//                         var confirmed = countryGeoJSON["features"][jsonEntry]["properties"][jsonDate][0];
-//                         var deaths = countryGeoJSON["features"][jsonEntry]["properties"][jsonDate][1];
-//                         var recovered = countryGeoJSON["features"][jsonEntry]["properties"][jsonDate][2];
-                        
-//                         var active = confirmed - deaths - recovered;
-//                         countryGeoJSON["features"][jsonEntry]["properties"][jsonDate].push(active);
-//                     }
-//                 }
-//             };
-//         };
-//     }
-// }
-
-
-
+// EOF
 
 
 

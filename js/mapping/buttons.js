f80b12d6139806d0e64960237cfc199d0c5fb84d
Andrew Figueiredo <andrewfigueiredo@Andrews-MacBook.local>
2020-04-05 20:34:37 -0400

    merge required

--- js/mapping/buttons.js
+++ js/mapping/buttons.js
@@ -1,9 +1,80 @@
+    map.on('load', function() {
+        map.addSource('museums', {
+            type: 'vector',
+            url: 'mapbox://mapbox.2opop9hr'
+        });
+        map.addLayer({
+            'id': 'museums',
+            'type': 'circle',
+            'source': 'museums',
+            'layout': {
+                'visibility': 'visible'
+            },
+            'paint': {
+                'circle-radius': 8,
+                'circle-color': 'rgba(55,148,179,1)'
+            },
+            'source-layer': 'museum-cusco'
+        });
 
+        map.addSource('contours', {
+            type: 'vector',
+            url: 'mapbox://mapbox.mapbox-terrain-v2'
+        });
+        map.addLayer({
+            'id': 'contours',
+            'type': 'line',
+            'source': 'contours',
+            'source-layer': 'contour',
+            'layout': {
+                'visibility': 'visible',
+                'line-join': 'round',
+                'line-cap': 'round'
+            },
+            'paint': {
+                'line-color': '#877b59',
+                'line-width': 1
+            }
+        });
+    });
+
+    var toggleableLayerIds = ['', 'museums'];
+
+    for (var i = 0; i < toggleableLayerIds.length; i++) {
+        var id = toggleableLayerIds[i];
+
+        var link = document.createElement('a');
+        link.href = '#';
+        link.className = 'active';
+        link.textContent = id;
+
+        link.onclick = function(e) {
+            var clickedLayer = this.textContent;
+            e.preventDefault();
+            e.stopPropagation();
+
+            var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+            if (visibility === 'visible') {
+                map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+                this.className = '';
+            } else {
+                this.className = 'active';
+                map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+            }
+        };
+
+        var layers = document.getElementById('menu');
+        layers.appendChild(link);
+    }
+
+/*
     var filterGroup = document.getElementById('filter-group');
 
 //replace places variable with countryGeoJSON
 
         countryGeoJSON.features.forEach(function(feature) {
+            if
             var symbol = feature.properties['icon'];
             var layerID = 'poi-' + symbol;
 
@@ -43,3 +114,4 @@
             }
         });
     });
+*/

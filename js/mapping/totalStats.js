fd5d7c6811f7dd5d1f1b533e3b01ab540192272f
yuvrende <yuvrender.gill@mail.utoronto.ca>
2020-04-06 20:50:28 -0400

    Fixed some aspect ratio issues

--- js/mapping/totalStats.js
+++ js/mapping/totalStats.js
@@ -13,17 +13,22 @@ var topDiv = document.getElementById("total-stats");
 
 var row = document.createElement("div");
 row.className = "row";
+
 var div1 = document.createElement("div"); div1.className = "col-sm text-center";
 var div2 = document.createElement("div"); div2.className = "col-sm text-center";
 var div3 = document.createElement("div"); div3.className = "col-sm text-center";
 var div4 = document.createElement("div"); div4.className = "col-sm text-center";
 div1.innerHTML = '<h6 style = "color: #f7f7f7">Confirmed' + "</h6>  <h7 style = 'color: #0275d8'>" + covid_total_current["confirmed"] + "</h7>"; 
+div1.style = "margin-bottom:15px;";
 row.appendChild(div1);
 div2.innerHTML = '<h6 style = "color: #d9534f">Deaths'  + "</h6>  <h7 style = 'color: #0275d8'>" +  covid_total_current["deaths"] + "</h7>"; 
+div2.style = "margin-bottom:15px;";
 row.appendChild(div2);
 div3.innerHTML = '<h6 style = "color: #5cb85c">Recovered' + "</h6>  <h7 style = 'color: #0275d8'>" + covid_total_current["recovered"] + "</h7>"; 
+div3.style = "margin-bottom:15px;";
 row.appendChild(div3);
 div4.innerHTML = '<h6 style = "color: #f0ad4e">Active'  + "</h6>  <h7 style = 'color: #0275d8'>" + (covid_total_current["confirmed"] 
 - covid_total_current["deaths"] - covid_total_current["recovered"]) + "</h7>"; 
+div4.style = "margin-bottom:15px;";
 row.appendChild(div4);
 topDiv.appendChild(row);
